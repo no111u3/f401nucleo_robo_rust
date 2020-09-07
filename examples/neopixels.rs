126f2cb042d20156f1228f0aa069cc3aa740fc74
Boris Vinogradov <no111u3@gmail.com>
2020-09-07 14:39:38 +0300

    Update neopixels example to more work

--- examples/neopixels.rs
+++ examples/neopixels.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_halt;
+use panic_halt as _;
 
 use cortex_m_rt::entry;
 
@@ -27,7 +27,6 @@ fn main() -> ! {
     let cp = Peripherals::take().unwrap();
 
     let gpioa = p.GPIOA.split();
-    let gpiob = p.GPIOB.split();
     let rcc = p.RCC.constrain();
 
     let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
@@ -35,15 +34,13 @@ fn main() -> ! {
     let mut led = gpioa.pa5.into_push_pull_output();
     led.set_low().ok();
 
-    let sck = gpiob.pb3.into_alternate_af5();
-    let miso = gpioa.pa6.into_alternate_af5();
+    let sck = spi::NoSck;
+    let miso = spi::NoMiso;
     let mosi = gpioa.pa7.into_alternate_af5();
 
     let mut delay = Delay::new(cp.SYST, clocks);
 
     let spi = Spi::spi1(p.SPI1, (sck, miso, mosi), MODE, 3_000_000.hz(), clocks);
-
-    let mut ws = Ws2812::new_sk6812w(spi);
 
     let data = [
         Color {
@@ -78,6 +75,8 @@ fn main() -> ! {
         },
     ];
     let empty = [Color::default(); 5];
+
+    let mut ws = Ws2812::new_sk6812w(spi);
 
     loop {
         ws.write(data.iter().cloned()).unwrap();
